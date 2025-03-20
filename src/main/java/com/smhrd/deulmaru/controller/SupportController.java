76eeb043925a45ae05d8e8b0511ef2f5ca25ec2f
Syrhein <namhib@naver.com>
2025-03-20 17:56:51 +0900

    Footer 삽입 및 경로 문제 해결, support api key 이슈 해결

--- src/main/java/com/smhrd/deulmaru/controller/SupportController.java
+++ src/main/java/com/smhrd/deulmaru/controller/SupportController.java
@@ -1,5 +1,6 @@
 package com.smhrd.deulmaru.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller  // ✅ @RestController → @Controller로 변경
 @RequestMapping("/supportApi")  
 public class SupportController {
-
+	
+    @Value("${api.serviceKey}")
+    private String apiServiceKey;
+	
     @GetMapping("/support")
-    public String grantsPage() {
+    public String grantsPage(Model model) {
+    	model.addAttribute("serviceKey", apiServiceKey); // 뷰에서 사용하기 위해 추가
         return "supportApi/support";  // ✅ templates/support.html 반환
     }
     
     @GetMapping("/detail/{seq}")
     public String grantDetailPage(@PathVariable String seq, Model model) {
         model.addAttribute("seq", seq);
+        model.addAttribute("serviceKey", apiServiceKey); // 뷰에서 사용하기 위해 추가
         return "supportApi/support-detail";  // ✅ templates/support-detail.html 반환
     }
 }
